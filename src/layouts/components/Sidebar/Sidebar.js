a10b0539757690cb8026e08652fe769c5458303d
Chicken <tuannnhe163137@fpt.edu.vn>
2023-11-22 15:25:59 +0700

    Update sibar

--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -1,14 +1,33 @@
 import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
+import config from '~/config';
+import Menu, { MenuItem } from './Menu';
+import {
+  HomeActiveIcon,
+  HomeIcon,
+  LiveActiveIcon,
+  LiveIcon,
+  UserGroupActiveIcon,
+  UserGroupIcon,
+} from '~/components/Icons';
 
 const cx = classNames.bind(styles);
 
 function Sidebar() {
-    return (
-        <aside className={cx('wrapper')}>
-            <h2>Sidebar</h2>
-        </aside>
-    );
+  return (
+    <aside className={cx('wrapper')}>
+      <Menu>
+        <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+        <MenuItem
+          title="Following"
+          to={config.routes.following}
+          icon={<UserGroupIcon />}
+          activeIcon={<UserGroupActiveIcon />}
+        />
+        <MenuItem title="LIVE" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
+      </Menu>
+    </aside>
+  );
 }
 
 export default Sidebar;
